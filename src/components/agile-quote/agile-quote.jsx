c2ecb6e1c4c5ad4e09aefe7df90d4b7a518479d1
Adit Patel <MyFolder@Adits-MacBook-Air.local>
2018-09-05 08:29:52 -0500

    updated portfolio

--- src/components/agile-quote/agile-quote.jsx
+++ src/components/agile-quote/agile-quote.jsx
@@ -9,7 +9,11 @@ class AgileQuoteComponent extends Component {
           <div className="agile-quote">
             <p className="quote">
               <span className="quote-content">
-             "."
+             "I have three precious things which I hold fast and prize. 
+              The first is gentleness; 
+              the second is frugality; 
+              the third is humility, which keeps me from putting myself before others. 
+              Be gentle and you can be bold; be frugal and you can be liberal; avoid putting yourself before others and you can become a leader among men."
               </span>
               <span className="quote-author">- Lao Tzu</span>
             </p>
